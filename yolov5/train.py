907e565586d39a89ec48d59d9b5ccc04fbef606a
niehmanyo <1695028510@qq.com>
2023-06-13 18:11:46 +0800

    detect.py 用自己的训练结果：100% 正确率:10%

--- yolov5/train.py
+++ yolov5/train.py
@@ -440,7 +440,7 @@ def parse_opt(known=False):
     parser.add_argument('--cfg', type=str, default=ROOT / 'models/hand_gestures.yaml', help='model.yaml path') ## It is my model
     parser.add_argument('--data', type=str, default=ROOT / 'data/data_hand_gestures.yaml', help='dataset.yaml path')
     parser.add_argument('--hyp', type=str, default=ROOT / 'data/hyps/hyp.scratch-low.yaml', help='hyperparameters path')
-    parser.add_argument('--epochs', type=int, default=3, help='total training epochs')
+    parser.add_argument('--epochs', type=int, default=50, help='total training epochs')
     parser.add_argument('--batch-size', type=int, default=4, help='total batch size for all GPUs, -1 for autobatch')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='train, val image size (pixels)')
     parser.add_argument('--rect', action='store_true', help='rectangular training')
